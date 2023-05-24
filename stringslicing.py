ee25f3de05d38877a77fec355f6e6727425acabe
gnaneshw <134358930+gnaneshw@users.noreply.github.com>
2023-05-24 10:55:59 +0600

    Add files via upload

---
+++ stringslicing.py
@@ -0,0 +1,24 @@
+str1= '5'
+print(str1,is alnum())
+str2='a'
+print(str2,is alpha())
+str3='5'
+print(str3,is digit())
+str4='akp college'
+print(str3,is digit lower())
+str5='5658'
+print(str5,is numeric())
+str6=' '
+print(str6,is space())
+str7='A'
+print(str7,is title())
+str8='akp college'
+print(str8,is upper())
+str9='akp college'
+print(str9,replace('i','e'))
+str10='Akp college'
+print(str10,split())
+str11='Akp college'
+print(str11,count('a'))
+str12='Akp college'
+print(str12,find('g'))
